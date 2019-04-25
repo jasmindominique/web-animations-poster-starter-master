22a2cf099c128fd1819ac770ef24e48564e29988
jasmindominique <jsurpris.jd@gmail.com>
2019-04-25 09:27:11 -0400

    keyframe changes, scss changes, etc

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,20 @@
-"use strict";
+// let mountainBk = document.querySelector(".mountain-bk");
 
-console.log("Hello World from main.js!");
+// let mountainFr = document.querySelector(".mountain-fr");
+
+// let logo = document.querySelector(".logo");
+
+// let movieOne = document.querySelector(".movietitleone");
+
+// let movieTwo = document.querySelector(".movietitletwo");
+
+
+// let tl = new TimelineLite();
+
+// 	tl.to(mountainBk, 1, {y: 200}, 0.5);
+// 		.to(mountainFr, 1, {y:.5, }, "-=.25");
+// 		.from(logo, 1, {x:100, opacity:0}, 2);
+// 		.from(movieOne, 1, {scale: 0.5, opacity:0}, "+=5");
+// 		.from(movieTwo, 1, {scale: 0.5, opacity:0}, "-=5");
+"use strict";
 //# sourceMappingURL=main.js.map
